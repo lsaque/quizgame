7c79123add53b022deef9be3e44a100e84893bb7
Isaque Souza <isaquetoonix245@gmail.com>
2021-11-09 17:30:32 -0300

    [update] (style): [created and styled Navbar component]

---
+++ src/components/layout/Navbar/index.tsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import MenuIcon from '@mui/icons-material/MenuOutlined';
+
+import { ColorModeContext } from '../../../context/ColorModeContext';
+
+export default function Navbar() {
+
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Isaquiz or Is.a.quiz
+          </Typography>
+          <Button 
+            color="inherit" 
+          >
+            Login
+          </Button>
+          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+            {theme.palette.mode === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
