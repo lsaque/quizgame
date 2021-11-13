56eacd1463de4e42812fcbefd10338c4b775ea2c
Isaque Souza <isaquetoonix245@gmail.com>
2021-11-13 19:11:37 -0300

    [update] (style): [created and styled FormsHome component]

---
+++ src/components/forms/FormsHome/index.tsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react';
+import { Grid } from '@mui/material';
+import ArrowRight from '@mui/icons-material/EastRounded';
+
+import { FooterModeContext } from '../../../context/FooterModeContext';
+import InternalContainer from '../../layout/InternalGrid';
+import Input from '../../inputs/Input';
+import ButtonIcon from '../../inputs/ButtonIcon';
+import { NOT_FOUND } from '../../../utils/constants/routes.constants';
+
+const FormsHome: React.FC = () => {
+
+  const { setState: setGlobalState } = useContext(FooterModeContext);
+  const handleSubmit = () => setGlobalState({ isPagination: true });
+
+  return (
+    <InternalContainer>
+      <Grid container spacing={2} mt={3}>
+        <Grid item xs={9.6}>
+          <Input
+            label="How many questions?"
+            type="number"
+          />
+        </Grid>
+
+        <Grid item xs={2.4}
+          sx={{
+            display: { xs: 'flex', md: 'block' },
+            justifyContent: 'flex-end',
+          }}
+        >
+          <ButtonIcon 
+            onClick={async () => handleSubmit()} 
+            icon={<ArrowRight/>}
+            href={NOT_FOUND}
+          />
+        </Grid>
+      </Grid>
+    </InternalContainer>
+  );
+}
+
+export default FormsHome;
