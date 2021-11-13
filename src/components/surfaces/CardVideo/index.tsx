ef3f63a385da9047f884e2a5ed681d6d95db97aa
Isaque Souza <isaquetoonix245@gmail.com>
2021-11-13 19:13:00 -0300

    [update] (style): [created and styled CardVideo component]

---
+++ src/components/surfaces/CardVideo/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, CardMedia } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+
+import video from '../../../assets/videos/mindBlowing.mp4';
+import Container from '../../layout/InternalGrid';
+
+const CardVideo: React.FC = () => {
+
+  const theme = useTheme();
+  
+  return (
+    <Container>
+      <Card>
+        <CardMedia
+          controls
+          loop
+          muted
+          autoPlay
+          component="video"
+          image={video}
+          sx={{
+            objectFit: 'cover',
+            backgroundColor: 'background.default',
+            filter: theme.palette.mode === 'dark' ? 'invert(0)' : 'grayscale(1) invert(0.9)',
+          }}
+        />
+      </Card>
+    </Container>
+
+  );
+}
+
+export default CardVideo;
